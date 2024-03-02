20daee6217db834ac93610e9edef382ec58a8fe1
Henning Scheufler <henning.scheufler@web.de>
2024-03-02 14:35:42 +0100

    update build.sh it no longer install NEOFOAM_GPL (Kokkos is no longer found )

--- build.sh
+++ build.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # Run CMake to generate the build files
+# -DCMAKE_INSTALL_PREFIX=$PWD/NeoFOAM_GPL \
 cmake  -S . -B build  -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX=$PWD/NeoFOAM_GPL \
+        -DNEOFOAM_BUILD_TESTS=ON \
         -DKokkos_ENABLE_SERIAL=ON \
         -DKokkos_ENABLE_OPENMP=ON \
         -DKokkos_ENABLE_CUDA=ON
+        # -DKokkos_ARCH_NATIVE=ON \
+        # -DKokkos_ENABLE_AGGRESSIVE_VECTORIZATION=ON
 
 # Build the project using make
-cmake --build build --target install -j
+cmake --build build -j
 
-PATH=$PWD/NeoFOAM_GPL/bin:$PATH
+# PATH=$PWD/NeoFOAM_GPL/bin:$PATH
