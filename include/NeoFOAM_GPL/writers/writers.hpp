0694a05c3ed0f148947eddf3fc284f2f7543540f
Henning Scheufler <henning.scheufler@web.de>
2024-03-02 14:34:34 +0100

    added writers to visualize NeoFOAM fields with paraview

---
+++ include/NeoFOAM_GPL/writers/writers.hpp
@@ -0,0 +1,85 @@
+// SPDX-License-Identifier: GPLv-3.0
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+#pragma once
+
+#include "fvMesh.H"
+#include "NeoFOAM/fields/field.hpp"
+#include "NeoFOAM_GPL/conversion/convert.hpp"
+
+void write(NeoFOAM::scalarField& sf, const Foam::fvMesh &mesh, const std::string fieldName)
+{
+    Foam::volScalarField* field = mesh.getObjectPtr<Foam::volScalarField>(fieldName);
+    if (field)
+    {
+        // field is already present and needs to be updated
+        auto sf_host = sf.copyToHost().field();
+        Foam::scalarField& field_ref = field->ref();
+        for (int i = 0; i < field_ref.size(); i++)
+        {
+            field_ref[i] = sf_host[i];
+        }
+        field->write();
+    }
+    else
+    {
+        Foam::volScalarField foamField
+        (
+            Foam::IOobject
+            (
+                fieldName,
+                mesh.time().timeName(),
+                mesh,
+                Foam::IOobject::NO_READ,
+                Foam::IOobject::AUTO_WRITE
+            ),
+            mesh,
+            Foam::dimensionedScalar(Foam::dimless, 0)
+        );
+        auto sf_host = sf.copyToHost().field();
+        Foam::scalarField& field_ref = foamField.ref();
+        for (int i = 0; i < field_ref.size(); i++)
+        {
+            field_ref[i] = sf_host[i];
+        }
+        foamField.write();
+    }
+}
+
+void write(NeoFOAM::vectorField& sf, const Foam::fvMesh &mesh, const std::string fieldName)
+{
+    Foam::volVectorField* field = mesh.getObjectPtr<Foam::volVectorField>(fieldName);
+    if (field)
+    {
+        // field is already present and needs to be updated
+        NeoFOAM::span<NeoFOAM::vector> sf_host = sf.copyToHost().field();
+        Foam::vectorField& field_ref = field->ref();
+        for (int i = 0; i < field_ref.size(); i++)
+        {
+            field_ref[i] = convert(sf_host[i]);
+        }
+        field->write();
+    }
+    else
+    {
+        Foam::volVectorField foamField
+        (
+            Foam::IOobject
+            (
+                fieldName,
+                mesh.time().timeName(),
+                mesh,
+                Foam::IOobject::NO_READ,
+                Foam::IOobject::AUTO_WRITE
+            ),
+            mesh,
+            Foam::dimensionedVector(Foam::dimless, Foam::Zero)
+        );
+        auto sf_host = sf.copyToHost().field();
+        Foam::vectorField& field_ref = foamField.ref();
+        for (int i = 0; i < field_ref.size(); i++)
+        {
+            field_ref[i] = convert(sf_host[i]);
+        }
+        foamField.write();
+    }
+}
