d631960a407fc98bb3936f3e27898037438ceada
Henning Scheufler <henning.scheufler@web.de>
2024-04-14 12:58:35 +0200

    update format

--- include/FoamAdapter/conversion/convert.hpp
+++ include/FoamAdapter/conversion/convert.hpp
@@ -1,20 +1,19 @@
 // SPDX-License-Identifier: GPLv-3.0
 // SPDX-FileCopyrightText: 2023 NeoFOAM authors
 #pragma once
-#include "NeoFOAM/primitives/vector.hpp"
-#include "NeoFOAM/primitives/scalar.hpp"
 #include "NeoFOAM/primitives/label.hpp"
+#include "NeoFOAM/primitives/scalar.hpp"
+#include "NeoFOAM/primitives/vector.hpp"
 #include "volFields.H"
 
-namespace Foam
-{
+namespace Foam {
 
-NeoFOAM::Vector convert(const Foam::vector& Type);
-NeoFOAM::scalar convert(const Foam::scalar& Type);
+NeoFOAM::Vector convert(const Foam::vector &Type);
+NeoFOAM::scalar convert(const Foam::scalar &Type);
 
-NeoFOAM::label convert(const Foam::label& Type);
+NeoFOAM::label convert(const Foam::label &Type);
 
 // To Foam
-Foam::vector convert(const NeoFOAM::Vector& Type);
+Foam::vector convert(const NeoFOAM::Vector &Type);
 
 }; // namespace Foam
--- include/FoamAdapter/conversion/type_conversion.hpp
+++ include/FoamAdapter/conversion/type_conversion.hpp
@@ -2,58 +2,52 @@
 // SPDX-FileCopyrightText: 2023 NeoFOAM authors
 #pragma once
 
-#include "volFields.H"
-#include "NeoFOAM/core/executor/executor.hpp"
 #include "FoamAdapter/conversion/convert.hpp"
-#include "NeoFOAM/fields/FieldTypeDefs.hpp"
 #include "NeoFOAM/cellCentredFiniteVolume/fields/fvccVolField.hpp"
+#include "NeoFOAM/core/executor/executor.hpp"
+#include "NeoFOAM/fields/FieldTypeDefs.hpp"
+#include "volFields.H"
 
-namespace Foam
-{
-template<typename From>
-struct type_map {
+namespace Foam {
+template <typename From> struct type_map {};
 
+// Specializations of type_map for specific type mappings.
+template <>
+struct type_map<
+    Foam::GeometricField<Foam::scalar, Foam::fvPatchField, Foam::volMesh>> {
+  using container_type = NeoFOAM::fvccVolField<NeoFOAM::scalar>;
+  using mapped_type = NeoFOAM::scalar;
+};
+
+template <>
+struct type_map<
+    Foam::GeometricField<Foam::vector, Foam::fvPatchField, Foam::volMesh>> {
+  using container_type = NeoFOAM::fvccVolField<NeoFOAM::Vector>;
+  using mapped_type = NeoFOAM::Vector;
 };
 
 // Specializations of type_map for specific type mappings.
-template<>
-struct type_map<Foam::GeometricField<Foam::scalar, Foam::fvPatchField, Foam::volMesh>> {
-    using container_type = NeoFOAM::fvccVolField<NeoFOAM::scalar>;
-    using mapped_type = NeoFOAM::scalar;
-};
-
-template<>
-struct type_map<Foam::GeometricField<Foam::vector, Foam::fvPatchField, Foam::volMesh>> {
-    using container_type = NeoFOAM::fvccVolField<NeoFOAM::Vector>;
-    using mapped_type = NeoFOAM::Vector;
+template <> struct type_map<Foam::Field<Foam::scalar>> {
+  using container_type = NeoFOAM::Field<NeoFOAM::scalar>;
+  using mapped_type = NeoFOAM::scalar;
 };
 
 // Specializations of type_map for specific type mappings.
-template<>
-struct type_map<Foam::Field<Foam::scalar>> {
-    using container_type = NeoFOAM::Field<NeoFOAM::scalar>;
-    using mapped_type = NeoFOAM::scalar;
+template <> struct type_map<Foam::Field<Foam::vector>> {
+  using container_type = NeoFOAM::Field<NeoFOAM::Vector>;
+  using mapped_type = NeoFOAM::Vector;
 };
 
 // Specializations of type_map for specific type mappings.
-template<>
-struct type_map<Foam::Field<Foam::vector>> {
-    using container_type = NeoFOAM::Field<NeoFOAM::Vector>;
-    using mapped_type = NeoFOAM::Vector;
+template <> struct type_map<Foam::List<Foam::scalar>> {
+  using container_type = NeoFOAM::Field<NeoFOAM::scalar>;
+  using mapped_type = NeoFOAM::scalar;
 };
 
 // Specializations of type_map for specific type mappings.
-template<>
-struct type_map<Foam::List<Foam::scalar>> {
-    using container_type = NeoFOAM::Field<NeoFOAM::scalar>;
-    using mapped_type = NeoFOAM::scalar;
-};
-
-// Specializations of type_map for specific type mappings.
-template<>
-struct type_map<Foam::List<Foam::label>> {
-    using container_type = NeoFOAM::Field<NeoFOAM::label>;
-    using mapped_type = NeoFOAM::label;
+template <> struct type_map<Foam::List<Foam::label>> {
+  using container_type = NeoFOAM::Field<NeoFOAM::label>;
+  using mapped_type = NeoFOAM::label;
 };
 
 }; // namespace Foam
