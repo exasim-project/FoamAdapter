e2de9e5c323236e3712c500d8ea0d13b26746d7e
Henning Scheufler <henning.scheufler@web.de>
2024-04-14 12:55:29 +0200

    update format

--- include/FoamAdapter/writers/writers.hpp
+++ include/FoamAdapter/writers/writers.hpp
@@ -2,90 +2,63 @@
 // SPDX-FileCopyrightText: 2023 NeoFOAM authors
 #pragma once
 
+#include "FoamAdapter/conversion/convert.hpp"
+#include "NeoFOAM/fields/FieldTypeDefs.hpp"
 #include "fvMesh.H"
 #include "volFields.H"
-#include "NeoFOAM/fields/FieldTypeDefs.hpp"
-#include "FoamAdapter/conversion/convert.hpp"
 
-namespace Foam
-{
+namespace Foam {
 
-void write(NeoFOAM::scalarField& sf, const Foam::fvMesh &mesh, const std::string fieldName)
-{
-    Foam::volScalarField* field = mesh.getObjectPtr<Foam::volScalarField>(fieldName);
-    if (field)
-    {
-        // field is already present and needs to be updated
-        auto sf_host = sf.copyToHost().field();
-        Foam::scalarField& field_ref = field->ref();
-        for (int i = 0; i < field_ref.size(); i++)
-        {
-            field_ref[i] = sf_host[i];
-        }
-        field->write();
+void write(NeoFOAM::scalarField &sf, const Foam::fvMesh &mesh,
+           const std::string fieldName) {
+  Foam::volScalarField *field =
+      mesh.getObjectPtr<Foam::volScalarField>(fieldName);
+  if (field) {
+    // field is already present and needs to be updated
+    auto sf_host = sf.copyToHost().field();
+    Foam::scalarField &field_ref = field->ref();
+    for (int i = 0; i < field_ref.size(); i++) {
+      field_ref[i] = sf_host[i];
     }
-    else
-    {
-        Foam::volScalarField foamField
-        (
-            Foam::IOobject
-            (
-                fieldName,
-                mesh.time().timeName(),
-                mesh,
-                Foam::IOobject::NO_READ,
-                Foam::IOobject::AUTO_WRITE
-            ),
-            mesh,
-            Foam::dimensionedScalar(Foam::dimless, 0)
-        );
-        auto sf_host = sf.copyToHost().field();
-        Foam::scalarField& field_ref = foamField.ref();
-        for (int i = 0; i < field_ref.size(); i++)
-        {
-            field_ref[i] = sf_host[i];
-        }
-        foamField.write();
+    field->write();
+  } else {
+    Foam::volScalarField foamField(
+        Foam::IOobject(fieldName, mesh.time().timeName(), mesh,
+                       Foam::IOobject::NO_READ, Foam::IOobject::AUTO_WRITE),
+        mesh, Foam::dimensionedScalar(Foam::dimless, 0));
+    auto sf_host = sf.copyToHost().field();
+    Foam::scalarField &field_ref = foamField.ref();
+    for (int i = 0; i < field_ref.size(); i++) {
+      field_ref[i] = sf_host[i];
     }
+    foamField.write();
+  }
 }
 
-void write(NeoFOAM::vectorField& sf, const Foam::fvMesh &mesh, const std::string fieldName)
-{
-    Foam::volVectorField* field = mesh.getObjectPtr<Foam::volVectorField>(fieldName);
-    if (field)
-    {
-        // field is already present and needs to be updated
-        auto sf_host = sf.copyToHost().field();
-        Foam::vectorField& field_ref = field->ref();
-        for (int i = 0; i < field_ref.size(); i++)
-        {
-            field_ref[i] = convert(sf_host[i]);
-        }
-        field->write();
+void write(NeoFOAM::vectorField &sf, const Foam::fvMesh &mesh,
+           const std::string fieldName) {
+  Foam::volVectorField *field =
+      mesh.getObjectPtr<Foam::volVectorField>(fieldName);
+  if (field) {
+    // field is already present and needs to be updated
+    auto sf_host = sf.copyToHost().field();
+    Foam::vectorField &field_ref = field->ref();
+    for (int i = 0; i < field_ref.size(); i++) {
+      field_ref[i] = convert(sf_host[i]);
     }
-    else
-    {
-        Foam::volVectorField foamField
-        (
-            Foam::IOobject
-            (
-                fieldName,
-                mesh.time().timeName(),
-                mesh,
-                Foam::IOobject::NO_READ,
-                Foam::IOobject::AUTO_WRITE
-            ),
-            mesh,
-            Foam::dimensionedVector(Foam::dimless, Foam::Zero)
-        );
-        auto sf_host = sf.copyToHost().field();
-        Foam::vectorField& field_ref = foamField.ref();
-        for (int i = 0; i < field_ref.size(); i++)
-        {
-            field_ref[i] = convert(sf_host[i]);
-        }
-        foamField.write();
+    field->write();
+  } else {
+    Foam::volVectorField foamField(
+        Foam::IOobject(fieldName, mesh.time().timeName(), mesh,
+                       Foam::IOobject::NO_READ, Foam::IOobject::AUTO_WRITE),
+        mesh, Foam::dimensionedVector(Foam::dimless, Foam::Zero));
+    auto sf_host = sf.copyToHost().field();
+    Foam::vectorField &field_ref = foamField.ref();
+    for (int i = 0; i < field_ref.size(); i++) {
+      field_ref[i] = convert(sf_host[i]);
     }
+    foamField.write();
+  }
 }
 
 } // namespace Foam
--- src/treeAMR/treeAMRMesh.hpp
+++ src/treeAMR/treeAMRMesh.hpp
@@ -6,30 +6,32 @@
 
 #include <t8.h>
 #include <t8_cmesh.h>
-#include <t8_forest/t8_forest_general.h>            /* forest definition and general interface. */
+#include <t8_forest/t8_forest_general.h> /* forest definition and general interface. */
 #include <t8_forest/t8_forest_io.h>                 /* forest io interface. */
 #include <t8_schemes/t8_default/t8_default_cxx.hxx> /* default refinement scheme. */
 
-struct t8_step3_adapt_data
-{
-  double midpoint[3];               /* The midpoint of our sphere. */
-  double refine_if_inside_radius;   /* if an element's center is smaller than this value, we refine the element. */
-  double coarsen_if_outside_radius; /* if an element's center is larger this value, we coarsen its family. */
+struct t8_step3_adapt_data {
+  double midpoint[3];             /* The midpoint of our sphere. */
+  double refine_if_inside_radius; /* if an element's center is smaller than this
+                                     value, we refine the element. */
+  double coarsen_if_outside_radius; /* if an element's center is larger this
+                                       value, we coarsen its family. */
 };
 
-int t8_step3_adapt_callback(t8_forest_t forest, t8_forest_t forest_from, t8_locidx_t which_tree, t8_locidx_t lelement_id,
-                            t8_eclass_scheme_c *ts, const int is_family, const int num_elements, t8_element_t *elements[]);
-
+int t8_step3_adapt_callback(t8_forest_t forest, t8_forest_t forest_from,
+                            t8_locidx_t which_tree, t8_locidx_t lelement_id,
+                            t8_eclass_scheme_c *ts, const int is_family,
+                            const int num_elements, t8_element_t *elements[]);
 
 /**
  * @class treeAMRMesh
  * @brief Represents a mesh for tree-based adaptive mesh refinement (AMR).
  *
- * The treeAMRMesh class provides functionality for refining, coarsening, balancing, and writing the mesh.
- * It also provides information about the number of elements in the mesh.
+ * The treeAMRMesh class provides functionality for refining, coarsening,
+ * balancing, and writing the mesh. It also provides information about the
+ * number of elements in the mesh.
  */
-class treeAMRMesh
-{
+class treeAMRMesh {
 public:
   /**
    * @brief Constructor for treeAMRMesh.
@@ -78,9 +80,9 @@ private:
   int32_t maxLevel_;     ///< The maximum level of refinement for the mesh.
   int32_t nElements_;    ///< The number of elements in the mesh.
 
-  t8_cmesh_t cmesh_;             ///< The underlying mesh data structure.
-  t8_forest_t forest_;           ///< The underlying forest data structure.
-  t8_scheme_cxx_t *scheme;       ///< The scheme used for mesh operations.
+  t8_cmesh_t cmesh_;       ///< The underlying mesh data structure.
+  t8_forest_t forest_;     ///< The underlying forest data structure.
+  t8_scheme_cxx_t *scheme; ///< The scheme used for mesh operations.
 };
 
 #endif // TREEAMRMESH_HPP
