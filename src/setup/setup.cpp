7f2aff871f21bf92de26f0d0a83af560eca5d73d
Henning Scheufler <henning.scheufler@web.de>
2024-07-25 15:09:28 +0200

    cmake uses openfoam targets and fix compilate errors

--- src/setup/setup.cpp
+++ src/setup/setup.cpp
@@ -68,11 +68,11 @@ std::unique_ptr<Foam::fvMesh> Foam::createMesh(const Foam::Time& runTime)
 NeoFOAM::Executor Foam::createExecutor(const Foam::dictionary& dict)
 {
     auto exec_name = dict.get<Foam::word>("executor");
-    if (exec_name == "CPU")
+    if (exec_name == "Serial")
     {
         return NeoFOAM::SerialExecutor();
     }
-    else if (exec_name == "OMP")
+    else if (exec_name == "CPU")
     {
         return NeoFOAM::CPUExecutor();
     }
@@ -84,4 +84,5 @@ NeoFOAM::Executor Foam::createExecutor(const Foam::dictionary& dict)
     {
         Foam::FatalError << "Executor not recognized" << Foam::endl;
     }
+    return NeoFOAM::SerialExecutor();
 }
