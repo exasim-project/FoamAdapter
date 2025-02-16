336224a3ce734628fe5b7ac7103895bb9464d609
Henning Scheufler <henning.scheufler@web.de>
2025-02-16 13:34:08 +0100

    WIP: added icoNeoFOAM

---
+++ examples/icoNeoFoam/icoNeoFoam.cpp
@@ -0,0 +1,152 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 NeoFOAM authors
+
+#include "NeoFOAM/NeoFOAM.hpp"
+
+#include "FoamAdapter/FoamAdapter.hpp"
+#include "FoamAdapter/readers/foamDictionary.hpp"
+
+
+#define namespaceFoam
+#include "fvCFD.H"
+
+using Foam::Info;
+using Foam::endl;
+using Foam::nl;
+namespace fvc = Foam::fvc;
+namespace fvm = Foam::fvm;
+
+namespace dsl = NeoFOAM::dsl;
+namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char* argv[])
+{
+    Kokkos::initialize(argc, argv);
+    {
+#include "addCheckCaseOptions.H"
+#include "setRootCase.H"
+#include "createTime.H"
+
+        NeoFOAM::Database db;
+
+        fvcc::FieldCollection& fieldCollection =
+            fvcc::FieldCollection::instance(db, "fieldCollection");
+
+
+        NeoFOAM::Dictionary controlDict = Foam::readFoamDictionary(runTime.controlDict());
+        NeoFOAM::Executor exec = createExecutor(runTime.controlDict());
+
+        std::unique_ptr<Foam::MeshAdapter> meshPtr = Foam::createMesh(exec, runTime);
+        Foam::MeshAdapter& mesh = *meshPtr;
+
+        Foam::pisoControl piso(mesh);
+#include "createControl.H"
+
+        auto [adjustTimeStep, maxCo, maxDeltaT] = Foam::timeControls(runTime);
+
+#include "createFields.H"
+
+
+        std::tie(adjustTimeStep, maxCo, maxDeltaT) = timeControls(runTime);
+
+        Foam::scalar coNum = Foam::calculateCoNum(phi);
+        if (adjustTimeStep)
+        {
+            Foam::setDeltaT(runTime, maxCo, coNum, maxDeltaT);
+        }
+
+
+        NeoFOAM::Dictionary fvSchemesDict = Foam::readFoamDictionary(mesh.schemesDict());
+        NeoFOAM::Dictionary fvSolutionDict = Foam::readFoamDictionary(mesh.solutionDict());
+
+        Info << "creating NeoFOAM mesh" << endl;
+        NeoFOAM::UnstructuredMesh& nfMesh = mesh.nfMesh();
+
+        Info << "creating NeoFOAM fields" << endl;
+        fvcc::VolumeField<NeoFOAM::scalar>& nfT =
+            fieldCollection.registerField<fvcc::VolumeField<NeoFOAM::scalar>>(
+                Foam::CreateFromFoamField<Foam::volScalarField> {
+                    .exec = exec,
+                    .nfMesh = nfMesh,
+                    .foamField = T,
+                    .name = "nfT"
+                }
+            );
+        auto nfPhi0 = Foam::constructSurfaceField(exec, nfMesh, phi0);
+        auto nfPhi = Foam::constructSurfaceField(exec, nfMesh, phi);
+
+        Foam::scalar endTime = controlDict.get<Foam::scalar>("endTime");
+
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+        Info << "\nStarting time loop\n" << endl;
+
+        while (runTime.loop())
+        {
+            Info << "Time = " << runTime.timeName() << nl << endl;
+
+            // #include "CourantNo.H"
+
+            // Momentum predictor
+
+            Foam::fvVectorMatrix UEqn(fvm::ddt(U) + fvm::div(phi, U) - fvm::laplacian(nu, U));
+
+            if (piso.momentumPredictor())
+            {
+                solve(UEqn == -fvc::grad(p));
+            }
+
+            // --- PISO loop
+            while (piso.correct())
+            {
+                Foam::volScalarField rAU(1.0 / UEqn.A());
+                Foam::volVectorField HbyA(constrainHbyA(rAU * UEqn.H(), U, p));
+                Foam::surfaceScalarField phiHbyA(
+                    "phiHbyA",
+                    fvc::flux(HbyA) + fvc::interpolate(rAU) * fvc::ddtCorr(U, phi)
+                );
+
+                Foam::adjustPhi(phiHbyA, U, p);
+
+                // Update the pressure BCs to ensure flux consistency
+                Foam::constrainPressure(p, U, phiHbyA, rAU);
+
+                // Non-orthogonal pressure corrector loop
+                while (piso.correctNonOrthogonal())
+                {
+                    // Pressure corrector
+
+                    Foam::fvScalarMatrix pEqn(fvm::laplacian(rAU, p) == fvc::div(phiHbyA));
+
+                    pEqn.setReference(pRefCell, pRefValue);
+
+                    pEqn.solve(p.select(piso.finalInnerIter()));
+
+                    if (piso.finalNonOrthogonalIter())
+                    {
+                        phi = phiHbyA - pEqn.flux();
+                    }
+                }
+
+                // #include "continuityErrs.H"
+
+                U = HbyA - rAU * fvc::grad(p);
+                U.correctBoundaryConditions();
+            }
+
+            runTime.write();
+
+            runTime.printExecutionTime(Info);
+        }
+
+        Info << "End\n" << endl;
+    }
+    Kokkos::finalize();
+
+    return 0;
+}
+
+// ************************************************************************* //
