cb640b0b14f8709a3c4097fde8db3bc11d244060
Henning Scheufler <henning.scheufler@web.de>
2025-02-22 13:49:52 +0100

    added HeatTransfer Example

---
+++ examples/heatTransfer/heatTransfer.cpp
@@ -0,0 +1,123 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2025 NeoFOAM authors
+
+#include "NeoFOAM/NeoFOAM.hpp"
+
+#include "FoamAdapter/FoamAdapter.hpp"
+#include "FoamAdapter/readers/foamDictionary.hpp"
+
+
+#include "fvCFD.H"
+#include "pisoControl.H"
+
+using Foam::Info;
+using Foam::endl;
+using Foam::nl;
+namespace fvc = Foam::fvc;
+namespace fvm = Foam::fvm;
+
+namespace dsl = NeoFOAM::dsl;
+namespace fvcc = NeoFOAM::finiteVolume::cellCentred;
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char* argv[])
+{
+    Kokkos::initialize(argc, argv);
+    {
+#include "addCheckCaseOptions.H"
+#include "setRootCase.H"
+#include "createTime.H"
+
+        NeoFOAM::Database db;
+
+        fvcc::FieldCollection& fieldCollection =
+            fvcc::FieldCollection::instance(db, "fieldCollection");
+
+
+        NeoFOAM::Dictionary controlDict = Foam::readFoamDictionary(runTime.controlDict());
+        NeoFOAM::Executor exec = createExecutor(runTime.controlDict());
+
+        std::unique_ptr<Foam::MeshAdapter> meshPtr = Foam::createMesh(exec, runTime);
+        Foam::MeshAdapter& mesh = *meshPtr;
+
+        Foam::pisoControl piso(mesh);
+
+        auto [adjustTimeStep, maxCo, maxDeltaT] = Foam::timeControls(runTime);
+
+#include "createFields.H"
+
+
+        NeoFOAM::Dictionary fvSchemesDict = Foam::readFoamDictionary(mesh.schemesDict());
+        NeoFOAM::Dictionary fvSolutionDict = Foam::readFoamDictionary(mesh.solutionDict());
+
+        Info << "creating NeoFOAM mesh" << endl;
+        NeoFOAM::UnstructuredMesh& nfMesh = mesh.nfMesh();
+
+        Info << "creating NeoFOAM fields" << endl;
+        fvcc::VolumeField<NeoFOAM::scalar>& nfT =
+            fieldCollection.registerField<fvcc::VolumeField<NeoFOAM::scalar>>(
+                Foam::CreateFromFoamField<Foam::volScalarField> {
+                    .exec = exec,
+                    .nfMesh = nfMesh,
+                    .foamField = T,
+                    .name = "nfT"
+                }
+            );
+        auto& nfTOld = oldTime(nfT);
+        nfTOld.internalField() = nfT.internalField();
+        nfT.correctBoundaryConditions();
+
+        auto nfKappa = Foam::constructSurfaceField(exec, nfMesh, kappa);
+
+
+        Foam::scalar endTime = controlDict.get<Foam::scalar>("endTime");
+
+
+        // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+        Info << "\nStarting time loop\n" << endl;
+
+        while (runTime.loop())
+        {
+
+            runTime++;
+
+            Foam::scalar t = runTime.time().value();
+            Foam::scalar dt = runTime.deltaT().value();
+
+            Info << "Time = " << runTime.timeName() << nl << endl;
+
+            Foam::fvScalarMatrix TEqn(fvm::ddt(T) - fvm::laplacian(kappa, T));
+
+            TEqn.solve();
+
+            dsl::Expression nfTEqn(dsl::imp::ddt(nfT) - dsl::imp::laplacian(nfKappa, nfT));
+
+            dsl::solve(
+                nfTEqn,
+                nfT,
+                t,
+                dt,
+                fvSchemesDict,
+                fvSolutionDict.get<NeoFOAM::Dictionary>("solvers").get<NeoFOAM::Dictionary>("nfT")
+            );
+
+            runTime.write();
+            if (runTime.outputTime())
+            {
+                Info << "writing nfT field" << endl;
+                write(nfT.internalField(), mesh, "nfT");
+            }
+
+            runTime.printExecutionTime(Info);
+        }
+
+        Info << "End\n" << endl;
+    }
+    Kokkos::finalize();
+
+    return 0;
+}
+
+// ************************************************************************* //
