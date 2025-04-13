28146900e69b63f3f0a759c3cef1e7a6b5dd3c62
Henning Scheufler <henning.scheufler@web.de>
2025-04-13 20:29:59 +0200

    added explicitOperator benchmark

---
+++ benchmarks/catch_main.hpp
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// SPDX-FileCopyrightText: 2023 NeoFOAM authors
+
+
+#include "Kokkos_Core.hpp"
+
+#include <catch2/benchmark/catch_benchmark.hpp>
+#include <catch2/catch_session.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators_all.hpp>
+#include <catch2/matchers/catch_matchers_all.hpp>
+#include <catch2/catch_approx.hpp>
+#include <catch2/reporters/catch_reporter_registrars.hpp>
+#include <catch2/reporters/catch_reporter_streaming_base.hpp>
+
+
+#include "FoamAdapter/setup.hpp"
+
+#include "argList.H"
+#include "fvMesh.H"
+#include "Time.H"
+
+Foam::Time* timePtr;    // A single time object
+Foam::argList* argsPtr; // Some forks want argList access at createMesh.H
+
+
+int main(int argc, char* argv[])
+{
+    // Kokkos::initialize(argc, argv);
+    Kokkos::ScopeGuard guard(argc, argv);
+    Catch::Session session;
+
+    // Specify command line options
+    int returnCode = session.applyCommandLine(argc, argv);
+    if (returnCode != 0) // Indicates a command line error
+        return returnCode;
+
+    // Find position of separator "---"
+    int sepIdx = argc - 1;
+    for (int i = 1; i < argc; i++)
+    {
+        if (strcmp(argv[i], "---") == 0) sepIdx = i;
+    }
+
+    // Figure out argc for each part
+    int doctestArgc = (sepIdx == argc - 1) ? argc : sepIdx;
+    int foamArgc = (sepIdx == argc - 1) ? 1 : argc - sepIdx;
+
+    // Prepare argv for doctestArgv
+    char* doctestArgv[doctestArgc];
+    for (int i = 0; i < doctestArgc; i++)
+    {
+        doctestArgv[i] = argv[i];
+    }
+
+    // Prepare argv for OpenFOAM
+    char* foamArgv[foamArgc];
+    foamArgv[0] = argv[0];
+    for (int i = 1; i < foamArgc; i++)
+    {
+        foamArgv[i] = argv[doctestArgc + i];
+    }
+
+    // Overwrite argv and argc for Foam include files
+    argc = foamArgc;
+    for (int i = 1; i < foamArgc; i++)
+    {
+        argv[i] = foamArgv[i];
+    }
+
+#include "setRootCase.H"
+#include "createTime.H"
+
+    argsPtr = &args;
+    timePtr = &runTime;
+
+    int result = session.run();
+
+    // Run benchmarks if there are any
+    // Kokkos::finalize();
+
+    return result;
+}
