a973426a18c0f3fa5b737d2188191a96f55e470b
Gregor Olenik <gregor.olenik@web.de>
2025-05-01 13:19:36 +0200

    rename benchmarks, fix failing sparsity test

--- test/test_sparsityPattern.cpp
+++ test/test_sparsityPattern.cpp
@@ -37,16 +37,22 @@ TEST_CASE("sparsityPattern")
         const auto colIdxs = pattern.colIdxs().view();
         const auto rowPtrs = pattern.rowOffs().view();
 
-        for (auto celli = 0; mesh.cellCells().size(); celli++)
+        REQUIRE(colIdxs.size() == mesh.nCells() + 2 * mesh.nInternalFaces());
+        REQUIRE(rowPtrs.size() == mesh.nCells() + 1);
+
+        for (auto celli = 0; celli < mesh.nCells(); celli++)
         {
             std::set<Foam::label> stencilCells;
             stencilCells.insert(celli);
+
+            // get cells connected to celli
             for (auto neiCelli : mesh.cellCells()[celli])
             {
                 stencilCells.insert(neiCelli);
             }
 
-            for (std::size_t i = rowPtrs[celli]; i < rowPtrs[celli + 1]; i++)
+            // test if columns in corresponding rows are in stencil cells set
+            for (auto i = rowPtrs[celli]; i < rowPtrs[celli + 1]; i++)
             {
                 auto colIdx = colIdxs[i];
                 REQUIRE(stencilCells.contains(colIdx));
