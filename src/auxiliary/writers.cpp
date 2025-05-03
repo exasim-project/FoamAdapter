6247f6f36f6acbf3eee1e7d6895090b592728b6e
Henning Scheufler <henning.scheufler@web.de>
2025-05-03 19:34:52 +0200

    ENH: write VolumeField with boundaryValues

--- src/auxiliary/writers.cpp
+++ src/auxiliary/writers.cpp
@@ -55,7 +55,83 @@ void write(NeoN::Vector<NeoN::Vec3>& sf, const Foam::fvMesh& mesh, const std::st
             Foam::dimensionedVector(Foam::dimless, Foam::Zero)
         );
         detail::copy_impl(foamField.ref(), sf);
+
+
         foamField.write();
     }
 }
+
+void write(
+    const fvcc::VolumeField<NeoN::scalar>& volField,
+    const Foam::fvMesh& mesh,
+    const std::string fieldName
+)
+{
+
+    Foam::volScalarField foamField(
+        Foam::IOobject(
+            fieldName,
+            mesh.time().timeName(),
+            mesh,
+            Foam::IOobject::NO_READ,
+            Foam::IOobject::AUTO_WRITE
+        ),
+        mesh,
+        Foam::dimensionedScalar(Foam::dimless, Foam::Zero)
+    );
+    auto hostVolField = volField.internalVector().copyToHost();
+    detail::copy_impl(foamField.ref(), hostVolField);
+
+    auto hostBCValue = volField.boundaryData().value().copyToHost();
+
+    forAll(foamField.boundaryField(), patchi)
+    {
+        Foam::fvPatchScalarField& foamFieldPatch = foamField.boundaryFieldRef()[patchi];
+        auto [start, end] = volField.boundaryData().range(patchi);
+
+        forAll(foamFieldPatch, bfacei)
+        {
+            foamFieldPatch[bfacei] = hostBCValue.view()[start + bfacei];
+        }
+    }
+    foamField.write();
+}
+
+void write(
+    const fvcc::VolumeField<NeoN::Vec3>& volField,
+    const Foam::fvMesh& mesh,
+    const std::string fieldName
+)
+{
+    Foam::volVectorField foamField(
+        Foam::IOobject(
+            fieldName,
+            mesh.time().timeName(),
+            mesh,
+            Foam::IOobject::NO_READ,
+            Foam::IOobject::AUTO_WRITE
+        ),
+        mesh,
+        Foam::dimensionedVector(Foam::dimless, Foam::Zero)
+    );
+    auto hostVolField = volField.internalVector().copyToHost();
+    detail::copy_impl(foamField.ref(), hostVolField);
+
+    auto hostBCValue = volField.boundaryData().value().copyToHost();
+
+    forAll(foamField.boundaryField(), patchi)
+    {
+        Foam::fvPatchVectorField& foamFieldPatch = foamField.boundaryFieldRef()[patchi];
+        auto [start, end] = volField.boundaryData().range(patchi);
+
+        forAll(foamFieldPatch, bfacei)
+        {
+            foamFieldPatch[bfacei][0] = hostBCValue.view()[start + bfacei][0];
+            foamFieldPatch[bfacei][1] = hostBCValue.view()[start + bfacei][1];
+            foamFieldPatch[bfacei][2] = hostBCValue.view()[start + bfacei][2];
+        }
+    }
+    foamField.write();
+}
+
 }
