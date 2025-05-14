f4a9a5f5ce2e7e65bb48a6bad2510a420f78a8c7
greole <greole@users.noreply.github.com>
2025-05-14 10:53:57 +0000

    Deploying to gh-pages from @ exasim-project/FoamAdapter@d535668aba3accf81123669d7ad6ce27e55f6f57 🚀

--- Build_PR_81/doxygen/html/search/functions_c.js
+++ Build_PR_81/doxygen/html/search/functions_c.js
@@ -3,5 +3,5 @@ var searchData=
   ['setdeltat_0',['setDeltaT',['../namespaceFoamAdapter.html#a52be9ed969f57da51a090c9df132990d',1,'FoamAdapter']]],
   ['setreference_1',['setReference',['../classFoamAdapter_1_1Expression.html#a8ed934777e6059677c60d31b9c2f4c40',1,'FoamAdapter::Expression']]],
   ['solve_2',['solve',['../classFoamAdapter_1_1Expression.html#a64c5b8b6cb311a71dbd4b9b148db4d91',1,'FoamAdapter::Expression']]],
-  ['sparsitypattern_3',['sparsitypattern',['../classFoamAdapter_1_1Expression.html#a3c5bbe9b5a0d393b5fec194e4682cc0d',1,'FoamAdapter::Expression::sparsityPattern()'],['../classFoamAdapter_1_1Expression.html#a855cf32c23315ea3cc202e7bb1cd89d5',1,'FoamAdapter::Expression::sparsityPattern() const']]]
+  ['sparsitypattern_3',['sparsityPattern',['../classFoamAdapter_1_1Expression.html#a855cf32c23315ea3cc202e7bb1cd89d5',1,'FoamAdapter::Expression']]]
 ];
