4579ea85d97c9c209c513c5fafbaa3fcb2449e79
Henning Scheufler <henning.scheufler@web.de>
2025-06-28 18:07:41 +0200

    update explicitOperators to foamlib

--- benchmarks/benchmarkSuite/explicitOperators/createStudy.py
+++ benchmarks/benchmarkSuite/explicitOperators/createStudy.py
@@ -4,12 +4,22 @@ from pathlib import Path
 
 from foamlib.preprocessing.parameter_study import csv_generator
 
-# baseCase
-root = Path(__file__).parent
-template_case = root / "baseCase"
 
-study = csv_generator(
-    csv_file=root / "parastudy.csv", template_case=template_case, output_folder=root / "Cases"
+root = Path(__file__).parent
+
+# 3DCube
+template_case = root / "templates/3DCube"
+study_cube = csv_generator(
+    csv_file=root / "parastudy_3DCube.csv", template_case=template_case, output_folder=root / "Cases"
 )
 
-study.create_study(study_base_folder=root)
+# 2DSquare
+template_case = root / "templates/2DSquare"
+study_square = csv_generator(
+    csv_file=root / "parastudy_2DSquare.csv", template_case=template_case, output_folder=root / "Cases"
+)
+
+# Combine both studies
+final_study = study_cube + study_square
+final_study.create_study(study_base_folder=root)
+
