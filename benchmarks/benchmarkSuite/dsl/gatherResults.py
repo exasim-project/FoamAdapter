66fcd602fba917678a99fd8c8bb551c459619336
Henning Scheufler <henning.scheufler@web.de>
2025-06-28 18:51:40 +0200

    benchmark dsl uses foamlib

---
+++ benchmarks/benchmarkSuite/dsl/gatherResults.py
@@ -0,0 +1,31 @@
+# %%
+from __future__ import annotations
+
+from pathlib import Path
+import pandas as pd
+
+from foamlib.postprocessing.load_tables import datafile, load_tables
+from foamlib.postprocessing.table_reader import read_catch2_benchmark
+
+root = Path(__file__).parent
+results = root / "results"
+results.mkdir(exist_ok=True)
+
+file = datafile(file_name="dsl.xml", folder=".")
+benchmark_results = load_tables(
+    source=file, dir_name=root / "Cases", reader_fn=read_catch2_benchmark
+)
+
+
+# save per test case
+def save_test_results(df: pd.DataFrame, test_case: str):
+    test_case_df = df[df["test_case"] == test_case]
+    if not test_case_df.empty:
+        test_case_df.to_csv(results / f"{test_case}.csv", index=False)
+
+
+for test_case in benchmark_results["test_case"].unique():
+    save_test_results(benchmark_results, test_case)
+
+
+# %%
