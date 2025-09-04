29374db7f2a95250199b047948db519b67fc0018
chihtaw <chihtaw@users.noreply.github.com>
2025-09-04 12:28:04 +0000

    Deploying to gh-pages from @ exasim-project/FoamAdapter@503f087bad9f08982b1abf2e16cfcb4fa4642afb 🚀

---
+++ Build_PR_101/_sources/testcases.rst.txt
@@ -0,0 +1,9 @@
+Test cases
+==========
+
+Currently the following testcases are provided within the FoamAdapter.
+
+Scalar advection
+^^^^^^^^^^^^^^^^
+
+Additionally, we provide several CMake presets to set commonly required flags if you compile NeoFoam in combination with Kokkos.
---
+++ Build_PR_101/doxygen/html/search/variables_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['name_0',['name',['../classFoamAdapter_1_1CreateFromFoamField.html#afb8e0535d4b118dce34d655b513b4c6f',1,'FoamAdapter::CreateFromFoamField']]],
+  ['nfmesh_1',['nfMesh',['../classFoamAdapter_1_1CreateFromFoamField.html#affd681043c1581bd6bc4db7adf46be0e',1,'FoamAdapter::CreateFromFoamField']]]
+];
