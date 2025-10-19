0f790c4232aeb045acf0480630156764933089e8
HenningScheufler <HenningScheufler@users.noreply.github.com>
2025-10-19 13:19:17 +0000

    Deploying to gh-pages from @ exasim-project/FoamAdapter@89bcc4714483ce38b75e83d328b4c103cdcac921 🚀

--- Build_PR_99/doxygen/html/search/variables_8.js
+++ Build_PR_99/doxygen/html/search/variables_8.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['subcycleindex_0',['subCycleIndex',['../classFoamAdapter_1_1CreateFromFoamField.html#a42bdc86ceec3e7f5a6ecaee3087f87a7',1,'FoamAdapter::CreateFromFoamField']]]
+  ['prefcell_5f_0',['pRefCell_',['../structFoamAdapter_1_1PDESolver_1_1SetReference.html#a3889bb614c28806145995a767cc2d162',1,'FoamAdapter::PDESolver::SetReference']]],
+  ['prefvalue_5f_1',['pRefValue_',['../structFoamAdapter_1_1PDESolver_1_1SetReference.html#a46a354ba29700cf6251c2925f8ea5255',1,'FoamAdapter::PDESolver::SetReference']]]
 ];
